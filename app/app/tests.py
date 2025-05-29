de36470e83a8cf02a9f9b90fdcbd74f562093573
fabianopapaiz <fabianopapaiz@gmail.com>
2025-05-28 23:14:50 -0300

    a

---
+++ app/app/tests.py
@@ -0,0 +1,10 @@
+from django.test import SimpleTestCase
+
+from app import calc
+
+class Calc_Test(SimpleTestCase):
+
+    def test_calc(self):
+        res =  calc.add_numbers(5, 6)
+
+        self.assertEquals(res, 11)
