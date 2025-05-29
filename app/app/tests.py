fb9aa8a843879e3ad337dae3fd71a92c4ac56058
fabianopapaiz <fabianopapaiz@gmail.com>
2025-05-28 23:19:00 -0300

    Update tests.py

--- app/app/tests.py
+++ app/app/tests.py
@@ -2,9 +2,10 @@ from django.test import SimpleTestCase
 
 from app import calc
 
+
 class Calc_Test(SimpleTestCase):
 
     def test_calc(self):
-        res =  calc.add_numbers(5, 6)
+        res = calc.add_numbers(5, 6)
 
         self.assertEquals(res, 11)
