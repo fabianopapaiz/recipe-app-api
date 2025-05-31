4490df3db6a0544942268542750b79b8c5b2f81a
fabianopapaiz <fabianopapaiz@gmail.com>
2025-05-31 07:52:00 -0300

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'app.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE'  : 'django.db.backends.postgresql',
-        'PORT': '5432',
+        'PORT': '5432', # porta usada dentro do container
         'HOST'    : os.environ.get('DB_HOST'),
         'NAME'    : os.environ.get('DB_NAME'),
         'USER'    : os.environ.get('DB_USER'),
