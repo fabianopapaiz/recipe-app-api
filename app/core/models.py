616d3a7b90679f839b5231b19c099e443569d0f1
fabianopapaiz <fabianopapaiz@gmail.com>
2025-06-05 09:49:04 -0300

    a

--- app/core/models.py
+++ app/core/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+from django.contrib.auth.models import (
+    AbstractBaseUser,
+    BaseUserManager,
+    PermissionsMixin,
+)  
+
+
+class UsuarioManager(BaseUserManager):
+
+    def create_usuario(self, email, password=None, **extra_fields):
+        usuario = self.model(email=email, **extra_fields)
+        usuario.set_password(password)
+        usuario.save(self._db)
+
+        return usuario
+
+
+class Usuario(AbstractBaseUser, PermissionsMixin):
+
+    email = models.EmailField(max_length=255, unique=True,blank=False, null=False)
+    nome = models.CharField(max_length=100)
+    ativo = models.BooleanField(default=True)
+    administrador = models.BooleanField(default=False)
+
+    objects = UsuarioManager()
+
+    USERNAME_FIELD = "email"
+
+    class Meta:
+        db_table = 'usuario'
