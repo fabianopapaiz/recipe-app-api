164ef5c9d1080982d631d71745085fba3bd4c16d
fabianopapaiz <fabianopapaiz@gmail.com>
2025-06-05 11:18:28 -0300

    a

--- app/core/models.py
+++ app/core/models.py
@@ -9,20 +9,37 @@ from django.contrib.auth.models import (
 
 class UsuarioManager(BaseUserManager):
 
-    def create_usuario(self, email, password=None, **extra_fields):
-        usuario = self.model(email=email, **extra_fields)
+    def create_user(self, email, password=None, **extra_fields):
+        if not email:
+            raise ValueError('O e-mail do usuário deve ser informado.')
+
+        usuario = self.model(
+            # normaliza os emails deixando tudo em minusculo
+            email=self.normalize_email(email), 
+            **extra_fields
+        )
         usuario.set_password(password)
         usuario.save(self._db)
 
         return usuario
 
 
+    def create_superuser(self, email, password):
+        usuario = self.create_user(email, password)
+        usuario.is_staff = True
+        usuario.is_superuser = True
+        usuario.save(using=self._db)
+
+        return usuario
+
+
+
 class Usuario(AbstractBaseUser, PermissionsMixin):
 
-    email = models.EmailField(max_length=255, unique=True,blank=False, null=False)
+    email = models.EmailField(max_length=255, unique=True ,blank=False, null=False)
     nome = models.CharField(max_length=100)
-    ativo = models.BooleanField(default=True)
-    administrador = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
     objects = UsuarioManager()
 
